253ec6c45c48a621537cb622b7286106eea8a15c
Saurabh Patil <81816560+saurabh-2411@users.noreply.github.com>
2022-09-26 22:05:23 +0530

    Add files via upload

---
+++ 26-9-22/MainPlayer.java
@@ -0,0 +1,24 @@
+public class MainPlayer {
+
+	public static void main(String[] args) {
+		Player p = new Player();
+		p.jersey();
+		
+		p = new Cricketer(); // upcasting
+		p.jersey();
+		
+		if (p instanceof Cricketer) {
+			((Cricketer) p).bat(); // downcasting
+			((Cricketer) p).ball();
+		}
+		
+		p = new Footballer(); // upcasting
+		p.jersey();
+		
+		if (p instanceof Footballer) {
+			((Footballer) p).goal(); // downcasting
+			((Footballer) p).defence();	
+		}
+	}
+
+}
