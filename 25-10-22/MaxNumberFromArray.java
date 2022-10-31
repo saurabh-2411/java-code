b165ba680dc47a45810665645312cef250138250
Saurabh Patil <81816560+saurabh-2411@users.noreply.github.com>
2022-10-31 12:47:29 +0530

    Add files via upload

---
+++ 25-10-22/MaxNumberFromArray.java
@@ -0,0 +1,12 @@
+public class MaxNumberFromArray {
+	public static void main(String[] args) {
+		int[] numarr = {100,25,56,95,32};
+		int tempMaxVal = 0;
+		for (int i = 0; i < numarr.length; i++) {
+			if (tempMaxVal < numarr[i]) {
+				tempMaxVal = numarr[i];
+			}
+		}
+		System.out.println(tempMaxVal);
+	}
+}
